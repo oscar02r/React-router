d17ba8144754bcbceefa3b3626619e8104520dbb
oscar02r <oscary.ramirez@gmail.com>
2020-03-22 21:44:39 +0100

    Use queryString

--- src/App.js
+++ src/App.js
@@ -1,48 +1,85 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Link} from 'react-router-dom'
+import { BrowserRouter, Route, NavLink, Link} from 'react-router-dom'
+import queryString from 'query-string'
 
 const Home = (props)=>{
-  console.log(props)
+ 
   return(
     <h1>Home</h1>
   )
  
 }
-const Hola = ()=>(
-   <h1>Hola mundo!</h1>
-)
+const Hola = ()=>( <h1>Hola mundo!</h1>)
 
-const Productos = ()=>(
-  <h1>Productos vendidos!</h1>
-)
+const Productos = (props)=>(
+ 
+  <div>
+      <h1>Productos</h1> 
+      <Link to='/productos/Hogar'> Hogar </Link>
+      <Link to='/productos/Gamer/1'> Gamer </Link>
+  </div>
+  )
+
+const Categoria = ({match})=>{
+  
+  return(
+  <h1>Categoria: { match.params.categoria}</h1>
+  )
+}
+
+const Ropa = ({location})=>{
+      const {color, talla} = queryString.parse(location.search)
+      return(
+        <div>
+          <h1>Ropa</h1>
+          <div> Color: {color}</div>
+          <div>Talla: {talla}</div>
+        </div>
+      )
+}
 
 const styles ={
        display:'flex',
        justifyContent:'space-around'
 }
 
+const NAVACTIVE = {
+      color:'orangered'
+}
+
 const Navigation = ()=>(
       <nav
         style={ styles}
       >
-        <Link to ={
-              {
-                pathname:'/',
-                search:'?ordernar=nombre',
-                hash:'#hash-otro',
-                state:{
-                  name:'Oscary Pro',
-                  age:26
-                }
-              }
-        }
-          
+        <NavLink 
+          to= '/' 
+          exact 
+          activeStyle={NAVACTIVE}
         >
-         Home
-         </Link>
-        <Link to='/hola'> Hola </Link>
-        <Link to='/productos' replace> Productos </Link>
+          Home 
+        </NavLink>
+
+        <NavLink 
+          to= '/hola'
+          activeClassName='navActive'
+        > 
+          Hola 
+        </NavLink>
+
+        <NavLink 
+          to= '/productos' 
+          activeStyle={NAVACTIVE}
+          isActive = {(match,location)=>{
+            if(!match) return false
+            return !match.isExact
+          }}
+        > 
+          Productos
+        </NavLink>
+        <NavLink to='/ropa' activeStyle={NAVACTIVE}>
+          Ropa
+        </NavLink>
       </nav>
 )
       
@@ -51,24 +88,12 @@ const Navigation = ()=>(
 function App() {
   return (
    <BrowserRouter>
-       <Navigation/>
+      <Navigation/>
       <Route path='/' exact component = {Home}/>
       <Route path='/hola' sensitive render = { Hola }/>
-      <Route 
-      path='/productos'
-      >
-      {
-        ({match})=>{
-          if(!match) return null
-          
-         return(
-          <Productos/>
-          )
-      }
-        
-      }
-        
-      </Route>
+      <Route path='/productos' exact render={Productos}/>
+      <Route path='/productos/:categoria/:id?' exact render={Categoria}/>
+      <Route path='/ropa' render={Ropa}/>
    </BrowserRouter>
   )
 }
