f5ddd3c3b909a117d55456cbe2590d500a4d9fe3
oscar02r <oscary.ramirez@gmail.com>
2020-03-22 15:10:39 +0100

    Use of link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Link} from 'react-router-dom'
 
-const Home = ()=>(
-  <h1>Home</h1>
-)
+const Home = (props)=>{
+  console.log(props)
+  return(
+    <h1>Home</h1>
+  )
+ 
+}
 const Hola = ()=>(
    <h1>Hola mundo!</h1>
 )
@@ -12,13 +16,47 @@ const Hola = ()=>(
 const Productos = ()=>(
   <h1>Productos vendidos!</h1>
 )
+
+const styles ={
+       display:'flex',
+       justifyContent:'space-around'
+}
+
+const Navigation = ()=>(
+      <nav
+        style={ styles}
+      >
+        <Link to ={
+              {
+                pathname:'/',
+                search:'?ordernar=nombre',
+                hash:'#hash-otro',
+                state:{
+                  name:'Oscary Pro',
+                  age:26
+                }
+              }
+        }
+          
+        >
+         Home
+         </Link>
+        <Link to='/hola'> Hola </Link>
+        <Link to='/productos' replace> Productos </Link>
+      </nav>
+)
+      
+
+
 function App() {
   return (
    <BrowserRouter>
+       <Navigation/>
       <Route path='/' exact component = {Home}/>
       <Route path='/hola' sensitive render = { Hola }/>
-
-      <Route path='/productos'>
+      <Route 
+      path='/productos'
+      >
       {
         ({match})=>{
           if(!match) return null
